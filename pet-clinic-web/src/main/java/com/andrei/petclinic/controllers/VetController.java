fba2bf64fc3d058798d08b51df22d6a75f988088
abalzan <andreibalzan@gmail.com>
2018-11-07 05:23:10 +0000

    creating index page and controller

---
+++ pet-clinic-web/src/main/java/com/andrei/petclinic/controllers/VetController.java
@@ -0,0 +1,16 @@
+package com.andrei.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
