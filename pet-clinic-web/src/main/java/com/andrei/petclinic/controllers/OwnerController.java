c2e0b6ff96d37b7010d9e68093deb9a61e7b311d
abalzan <andreibalzan@gmail.com>
2018-11-07 06:40:30 +0000

    returning list to the html.

--- pet-clinic-web/src/main/java/com/andrei/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/andrei/petclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.andrei.petclinic.controllers;
 
+import com.andrei.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @GetMapping({"", "/", "/index", "'/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
